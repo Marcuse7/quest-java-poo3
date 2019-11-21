1d18e545bea99d3557dde6d22580a6c9dd2b54a1
Markus Heinrichs <markus@heinrichs.es>
2019-11-21 16:29:53 +0100

    Add Fly.java, implement interface methods in Eagle.java

--- Eagle.java
+++ Eagle.java
@@ -21,4 +21,43 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+	public void takeOff() {
+		System.out.println(this.getName() + " takes off in the sky.");
+		
+	}
+
+	public void ascend(int i) {
+		this.altitude += i;
+		System.out.printf("%s flies upward, altitude: %d%n", this.getName(), altitude );
+		
+	}
+
+	public void glide() {
+		System.out.printf("%s glides into the air.%n", this.getName());
+		
+	}
+
+	public void descend(int i) {
+		if (i > altitude)
+			System.out.printf("%s is too high, it can't land.%n", this.getName());
+		else {
+			this.altitude -= i;
+			System.out.printf("%s flies downward, altitude: %d%n", this.getName(), altitude );
+		}
+			
+		
+	}
+
+	public void land() {
+		if (altitude > 1)
+			System.out.printf("%s is too high, it can't land.%n", this.getName());
+		else {
+			this.altitude = 0 ;
+			System.out.printf("%s lands on the ground.%n", this.getName() );
+		}
+	
+	}
+    
+    
 }
--- Nature.java
+++ Nature.java
@@ -11,17 +11,15 @@ public class Nature {
         pingou.swimUp(1);
         pingou.getOut();
 
-        Eagle eyeCherry = new Eagle("Eye Cherry");
-        // TODO : décommente le code suivant afin de le tester
-        /*
-        eyeCherry.takeOff();
-        eyeCherry.ascend(120);
-        eyeCherry.ascend(30);
-        eyeCherry.glide();
-        eyeCherry.descend(140);
-        eyeCherry.land();
-        eyeCherry.descend(9);
-        eyeCherry.land();
-        */
+        Eagle hawkeye = new Eagle("Hawkeye");
+        hawkeye.takeOff();
+        hawkeye.ascend(120);
+        hawkeye.ascend(30);
+        hawkeye.glide();
+        hawkeye.descend(140);
+        hawkeye.land();
+        hawkeye.descend(9);
+        hawkeye.land();
+        
     }
 }
